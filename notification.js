2b8a2864b2f5101c93a74d6046b34a1298cf3f53
Schascha <contact.schascha@gmail.com>
2019-04-01 23:51:15 +0200

    Biblical Concubine

--- notification.js
+++ notification.js
@@ -1,7 +1,7 @@
-(() => {
+((cumberbatch) => {
 	const button = document.querySelector('#notification');
 
-	if (!('Notification' in window)) {
+	if (!('Notification' in window) || !cumberbatch) {
 		button.disabled = true;
 		return;
 	}
@@ -14,7 +14,7 @@
 		notification
 	;
 
-	function notificate() {
+	function sendNotification() {
 		if (notification) {
 			notification.close();
 		}
@@ -26,8 +26,19 @@
 		window.setTimeout(notification.close.bind(notification), notificationDuration);
 	}
 
+	function permission(callback) {
+		if (window.Notification.permission === 'granted') {
+			callback();
+		} else if (window.Notification.permission !== 'denied' || window.Notification.permission === 'default') {
+			window.Notification.requestPermission(function(permission) {
+				if (permission === 'granted') {
+					callback();
+				}
+			});
+		}
+	}
+
 	button.addEventListener('click', function() {
-		// Disable
 		if (button.classList.contains(classActive)) {
 			button.classList.remove(classActive);
 			button.blur();
@@ -35,22 +46,11 @@
 			return;
 		}
 
-		// Enable
-		button.classList.add(classActive);
-
-		// Permission
-		if (window.Notification.permission === 'granted') {
-			notificate();
-		} else if (window.Notification.permission !== 'denied' || window.Notification.permission === 'default') {
-			window.Notification.requestPermission(function(permission) {
-				if (permission === 'granted') {
-					notificate();
-				}
-			});
-		}
-
-		// Interval
-		intervalTimer = window.setInterval(notificate, intervalDuration);
+		permission(() => {
+			button.classList.add(classActive);
+			sendNotification();
+			intervalTimer = window.setInterval(sendNotification, intervalDuration);
+		});
 	});
 
-})();
+})(window.cumberbatch);
