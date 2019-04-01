f3594bd586e92fa889f500054d302cd3057988d5
Schascha <contact.schascha@gmail.com>
2019-04-01 23:57:53 +0200

    Longdong Colonist

--- cumberbatch.js
+++ cumberbatch.js
@@ -1,7 +1,8 @@
-var cumberbatch = (() => {
+var cumberbatch = cumberbatch || (() => {
 	var
 		store = [],
 		index = 0,
+		memoryStore = {},
 		touchstartX = 0,
 		touchendX = 0,
 		touchDistance = 100
@@ -11,7 +12,7 @@ var cumberbatch = (() => {
 		names: {
 			first: [
 				"Bakery", "Bandersnatch", "Bandicoot", "Barbituate", "Barister", "Barnabus", "Barnacle", "Barnoldswick", "Baseballbat", "Baseballmitt", "Bedlington", "Beetlejuice", "Beezlebub", "Benadryl", "Bendandsnap", "Bendydick", "Benetton", "Benjamin", "Bentobox", "Bettyboop", "Billiardball", "Billybong", "Billyray", "Blasphemy", "Blenderdick", "Blubberbutt", "Blubberdick", "Blubberwhale", "Bodybuild", "Boilerdang", "Bombadil", "Bombadil", "Bonaparte", "Boobytrap", "Botany", "Bouillabaisse", "Bourgeoisie", "Brandenburg", "Brandybuck", "Brendadirk", "Brewery", "Broccoli", "Bubblebath", "Buckingham", "Buckminster", "Buckyball", "Budapest", "Buffalo", "Bukkake", "Bulbasaur", "Bumberstump", "Bumblebee", "Bumblebee", "Bumbleshack", "Bumblesniff", "Bunsenburner", "Burberry", "Burgerking", "Burlington", "Buttercup", "Butterfree", "Buttermilk", "Cadbury", "Cogglesnatch", "Congleton", "Danglerack", "Fiddlestick", "Fragglerock", "Honkytonk", "Johnnycash", "Liverswort", "Muffintop", "Oscarbait", "Pallettown", "Rinkydink", "Rumblesack", "Snorkeldink", "Snozzlebert", "Syphilis", "Tiddleywomp", "Timothy", "Wellington", "Whippersnatch", "Wimbledon",
-				"Bacardi", "Bagleface", "Barliman", "Bellybutton", "Benedick", "Berrywinkle", "Billyboy", "Booblesnoot", "Blueberry", "Blunderbrick", "Bobafett", "Bobbypin", "Brownbear", "Bumblesnuff", "Bumfuzzle", "Butterscotch", "Canterbury", "Carpettrip", "Cordula", "Dinglebird", "Dumbledore", "Engelbert", "Gobbledygook", "Hodgepodge", "Hullabaloo", "Hummingbird", "Igglybuff", "Lemontree", "Longdong", "Massachusetts", "Poppycock", "Quitsack", "Ragnarök", "Singlehandedly", "Stinkyfruit", "Stinkytoe","Taradiddle", "Widdershins"
+				"Bacardi", "Bagelbread", "Bagleface", "Barliman", "Bellybutton", "Benedick", "Berrywinkle", "Billyboy", "Booblesnoot", "Blueberry", "Blunderbrick", "Bobafett", "Bobbypin", "Brownbear", "Bumblesnuff", "Bumfuzzle", "Butterscotch", "Canterbury", "Carpettrip", "Cheesecake", "Cordula", "Dinglebird", "Dumbledore", "Engelbert", "Gobbledygook", "Hodgepodge", "Hullabaloo", "Hummingbird", "Igglybuff", "Lemontree", "Longdong", "Massachusetts", "Poppycock", "Quitsack", "Ragnarök", "Ronald", "Singlehandedly", "Stinkyfruit", "Stinkytoe", "Taradiddle", "Widdershins"
 			],
 			last: [
 				"Ampersand", "Anglerfish", "Animorph", "Banglesnatch", "Battleship", "Bumbersplat", "Cabbagepatch", "Calldispatch", "Cameltoe", "Camouflage", "Candlestick", "Candycrush", "Candygram", "Cankersore", "Capncrunch", "Carrotstick", "Charizard", "Charmander", "Cheddarcheese", "Chesterfield", "Chickenbroth", "Chickenstrips", "Chowderpants", "Chuckecheese", "Clavichord", "Clombyclomp", "Cockletit", "Coddleswort", "Coggleswort", "Collywog", "Colonist", "Commonwealth", "Concubine", "Coochierash", "Coochyrash", "Copperwire", "Cottagecheese", "Countryside", "Covergirl", "Crackerdong", "Crackerjack", "Crackersprout", "Cragglethatch", "Cramplesnutch", "Creamsicle", "Crimpysnitch", "Crucifix", "Crumplebutt", "Crumplehorn", "Crumplesack", "Cuckatoo", "Cuckooclock", "Cul-de-sac", "Cumberbitch", "Cumberbund", "Cumbercooch", "Cumbersnatch", "Cumbersniff", "Cumbersome", "Cummerbund", "Cunningsnatch", "Curdledmilk", "Curdledong", "Curdlemilk", "Curdlesnoot", "Custardbath", "Cuttlefish", "Frumblesnatch", "Humperdinck", "Kryptonite", "Lingerie", "Lumberjack", "Moldyspore", "Nottinghill", "Oxfordshire", "Pumpkinpatch", "Rivendell", "Scratch-n-sniff", "Scratchnsniff", "Snickersbar", "Snugglesnatch", "Splishnsplash", "Stinkyrash", "Talisman", "Toodlesnoot", "Upperclass", "Vegemite", "Wafflesmack",
@@ -36,12 +37,8 @@ var cumberbatch = (() => {
 		},
 
 		random() {
-			const
-				rand = (arr) => (arr) ? arr[parseInt(Math.random() * arr.length)] : Math.floor(Math.random() * 10) + 1,
-				name = (rand() == 10) ? rand(this.names.full) : rand(this.names.first) + ' ' + rand(this.names.last)
-			;
-
-			return name;
+			const rand = Math.floor(Math.random() * 10) + 1;
+			return (rand === 10) ? this._memory('full') : this._memory('first') + ' ' + this._memory('last');
 		},
 
 		generate() {
@@ -63,6 +60,26 @@ var cumberbatch = (() => {
 			if (index > 0) {
 				this._update(store[--index]);
 			}
+		},
+
+		_memory(name, limit = 20) {
+			const
+				size = this.names[name].length,
+				memory = memoryStore[name] || [],
+				arr = this.names[name].filter((el) => memory.indexOf(el) === -1),
+				str = arr[parseInt(Math.random() * arr.length)]
+			;
+
+			memory.push(str);
+
+			// Remove first element from memory if limit is reached
+			if (memory.length > ((limit > size) ? size / 2 : limit)) {
+				memory.shift();
+			}
+
+			memoryStore[name] = memory;
+
+			return str;
 		},
 
 		_update(name) {
